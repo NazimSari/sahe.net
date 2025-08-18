f86af682c12731826a736fd9a599f4d63499e9bb
Nazim <yasarnazimsari@gmail.com>
2025-08-18 23:13:00 +0300

    cors ayarı eklendi

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: ["http://localhost:3000", "https://sahe-net.vercel.app/"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
