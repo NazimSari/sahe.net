de38c7018a4c517be74147979fc2a370113fbe68
Nazim <yasarnazimsari@gmail.com>
2025-09-02 16:47:32 +0300

    cors options eklendi server.ts dosyasına

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,13 +11,19 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const corsOptions = {
-  origin: ["http://localhost:3000", "https://sahe-net.vercel.app"],
+  origin: [
+    "http://localhost:3000",
+    "https://sahe-net.vercel.app",
+    "https://www.sahe-net.vercel.app",
+  ],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   // credentials: true, // cookie/token göndereceksen aç
 };
 
 app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
+
 app.use(express.json());
 app.use(morgan("dev"));
 
